c994c979495616ff90128918141dd0ef76c82f54
Hanocha <saiki@feedforce.jp>
2018-07-02 12:27:31 +0900

    Implement sample iteration

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react';
 import { TodoItem } from './TodoItem';
 
+const sampleArray: string[] = ['test', 'test2', 'test3'];
+
 export default class App extends React.Component {
   public render() {
     return (
       <React.Fragment>
         <h1>Todo</h1>
-        <TodoItem text="test" />
+        {sampleArray.map(
+          (text, i) => <TodoItem text={text} key={i} />
+        )}
       </React.Fragment>
     );
-  }
-}
+  };
+};
