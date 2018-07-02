7aa97a1c8d4e2a23047016231dcde3e4bfbc5d0a
Hanocha <saiki@feedforce.jp>
2018-07-02 15:25:21 +0900

    Apply react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { TodoListContainer } from './TodoListContainer';
 
 export default class App extends React.Component {
   public render() {
     return (
-      <React.Fragment>
-        <h1>Todo</h1>
-        <TodoListContainer />
-      </React.Fragment>
+      <Router>
+        <React.Fragment>
+          <h1>Todo</h1>
+
+          <Route exact={true} path="/" component={TodoListContainer} />
+        </React.Fragment>
+      </Router>
     );
   };
 };
