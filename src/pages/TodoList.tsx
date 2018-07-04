14a8c1d5cd91dc6eb8ffdc45eb5ef086d3f2e2b2
otofu-square <yukita@feedforce.jp>
2018-07-04 16:41:46 +0900

    refactor: remove TodoListPage component & include it in pages/TodoList

--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import * as React from 'react';
-import { IProps as ITodo } from '../TodoItem';
-import { TodoListPage } from '../TodoListPage';
+import { Link } from 'react-router-dom';
+import { IProps as ITodo, TodoItem } from '../TodoItem';
 
 export interface IResponse {
   data: ITodo[];
@@ -24,6 +24,19 @@ export class TodoList extends React.Component<any, any, any> {
   }
 
   public render() {
-    return <TodoListPage todos={this.state.todos} />;
+    return (
+      <div>
+        <h1>Todo</h1>
+        <Link to="/new">Create</Link>
+        <ol>
+          {this.state.todos.map(todo => (
+            // @ts-ignore
+            <li key={todo.id}>
+              <TodoItem {...todo} />
+            </li>
+          ))}
+        </ol>
+      </div>
+    );
   }
 }
