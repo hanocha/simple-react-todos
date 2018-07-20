bc530da2a3a46c1d90d21c3fac8b1dcada2cc1d2
otofu-square <yukita@feedforce.jp>
2018-07-20 16:07:50 +0900

    refactor: use default export

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 
-export const App = () => (
+const App = () => (
   <div>
     <TodoForm />
     <TodoList />
   </div>
 );
+
+export default App;
